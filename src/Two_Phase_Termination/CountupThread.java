84f697a698edf864ab3a1222e3f2f28bf28b4aed
white3 <1070072593@qq.com>
2020-03-02 18:21:20 +0800

    The model of two phase termination

--- src/Two_Phase_Termination/CountupThread.java
+++ src/Two_Phase_Termination/CountupThread.java
@@ -1,11 +1,36 @@
 package Two_Phase_Termination;
 
-public class CountupThread extends Thread{
+public class CountupThread extends Thread {
+	private volatile boolean shutdownRequest = false;
+	private long counter = 0;
 
-	
+	public void shutdownRequest() {
+		shutdownRequest = true;
+		interrupt();
+	}
+
+	public boolean isShutdownRequest() {
+		return shutdownRequest;
+	}
+
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		super.run();
+		try {
+			while (!isShutdownRequest()) {
+				doWork();
+			}
+		} catch (InterruptedException e) {
+		} finally {
+			doShutdown();
+		}
+	}
+
+	private void doWork() throws InterruptedException {
+		System.out.println("work: counter = " + ++counter);
+		Thread.sleep(500);
+	}
+
+	private void doShutdown() {
+		System.out.println("doshutdown: counter = " + counter);
 	}
 }
